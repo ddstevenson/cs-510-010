e64dfbf5498fb6dccc2f65be4b34ed740813bf94
Slava Andrejev <vyac@pdx.edu>
2020-07-21 17:51:25 -0700

    Minimum test case

---
+++ psuftp/tests/connect.cpp
@@ -0,0 +1,11 @@
+#include <boost/test/test_tools.hpp>
+#include <boost/test/unit_test_log.hpp>
+#include <boost/test/unit_test_suite.hpp>
+
+BOOST_AUTO_TEST_SUITE(FTPConnectSuite)
+
+BOOST_AUTO_TEST_CASE(ConnectAnonIP4) {
+    BOOST_TEST(0 == 0);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
